1dda5a9a2bcfd8c794228f8f6998295da0194916
CJ Maxwell <christopher.j.maxwell.16@gmail.com>
2020-08-15 19:04:51 -0400

    + Went back and ported interactive mode to V5. Then used it to come up with a method to further solve puzzle states whose last 2 chars are switched (ex. U-V-W-Y-X, as happens during the solving of problem_board8). + Solved problem_board8, unsure if problem_board9 has a solution... + Added Node.ValueComparator & Node.compare function to order node values in the observed order used in the Kata creator's fixed tests (A-Z < 0-9 < a-z < {Symbols}) + Added solve(char[][] mb, char[][] sb, boolean debug = false) & solvePosition(int row, int col, char[][] sb, boolean debug = false) overloads to control whether to enter into interactive mode when a solution cannot be found for a given puzzle.

--- src/V5/Loopover.java
+++ src/V5/Loopover.java
@@ -3,6 +3,10 @@ package V5;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static java.lang.Character.*;
 
 public class Loopover {
     private static class Node{
@@ -60,6 +64,28 @@ public class Loopover {
         public String toString(){
             return Character.toString(value);
         }
+
+        private static int compare(Character a, Character b) {
+            // NOTE: different symbols might have different types
+            if (getType(a) == getType(b)) {
+                return a.compareTo(b);
+            }
+            // Symbols always come last
+            else if (isLetterOrDigit(a) ^ isLetterOrDigit(b)){
+                return isLetterOrDigit(a)? -1 : 1;
+            }
+            // Digits BEFORE Lowercase but AFTER Uppercase
+            else if (isLetter(a) ^ isLetter(b)){
+                return isUpperCase(a)? -1 : isLowerCase(b)? -1 : 1;
+            }
+            // Uppercase BEFORE Lowercase
+            else if (isUpperCase(a) ^ isUpperCase(b)) {
+                return isUpperCase(a) ? -1 : 1;
+            }
+            // Natural Comparision of 2 Symbols
+            return a.compareTo(b);
+        }
+        private static final Comparator<Character> ValueComparator = Node::compare;
     }
 
     private final HashMap<Character, Node> characterNodeHashMap;
@@ -255,8 +281,11 @@ public class Loopover {
                 System.out.println(this);
         }
     }
-    
-    private boolean solvePosition(int intended_row_num, int intended_col_num, char [][] solvedBoard){
+
+    private boolean solvePosition(int intended_row_num, int intended_col_num, char[][] solvedBoard){
+        return solvePosition(intended_row_num, intended_col_num, solvedBoard, false);
+    }
+    private boolean solvePosition(int intended_row_num, int intended_col_num, char[][] solvedBoard, boolean debug){
         char target_char = solvedBoard[intended_row_num][intended_col_num];
         Node target_node = characterNodeHashMap.get(target_char), displace_me = target_node, rc_handle;
         if (target_node.row_num == intended_row_num && target_node.col_num == intended_col_num)
@@ -282,8 +311,8 @@ public class Loopover {
                         displace_me = displace_me.up;
                     else displace_me = displace_me.down;
                 rc_handle = target_node.left;
-                moveToColumn(target_char, (intended_col_num + 1)%num_columns);
-                moveToColumn(displace_me.value, (intended_col_num + 1)%num_columns);
+                moveToColumn(target_char, target_node.right.col_num);
+                moveToColumn(displace_me.value, characterNodeHashMap.get(target_char).col_num);
                 moveToRow(target_char, intended_row_num);
                 moveToColumn(target_char, intended_col_num);
                 moveToColumn(rc_handle.value, intended_col_num);
@@ -294,7 +323,7 @@ public class Loopover {
             }
         }
         else if (intended_row_num < num_rows-1){
-            // make displace_me refer to node currently in the position being solved for
+            // make displace_me refer to node in the position being solved for
             while (displace_me.col_num != intended_col_num) {
                 if (displace_me.col_num > intended_col_num)
                     displace_me = displace_me.left;
@@ -313,7 +342,7 @@ public class Loopover {
             }
             else{
                 if (target_node.col_num == intended_col_num) {
-                    moveToColumn(target_char, (intended_col_num + 1)%num_columns);
+                    moveToColumn(target_char, target_node.right.col_num);
                 }
                 moveToRow(displace_me.value, target_node.row_num);
                 moveToColumn(target_char, intended_col_num);
@@ -331,20 +360,48 @@ public class Loopover {
                 moveToColumn(solvedBoard[intended_row_num][intended_col_num-1], num_columns-2);
                 moveToRow(target_char, intended_row_num);
                 moveToColumn(expat_value, num_columns-2);
-                Node sanchk = characterNodeHashMap.get(expat_value).up.right;
+                Node sanchk = characterNodeHashMap.get(expat_value).up;
+
                 for (int i=0; i < num_columns; i++) {
-                    if (sanchk.down.value > target_char)
+                    if (sanchk.right.down.value != expat_value && Node.ValueComparator.compare(sanchk.right.down.value, target_char) > 0)
                         break;
                     else
-                        moveToColumn(target_node.value, (characterNodeHashMap.get(target_node.value).col_num + 1) % num_columns);
+                        moveToColumn(target_node.value, target_node.right.col_num);
                 }
                 // FAIL-STATE Condition
-                if (sanchk.down.value > target_char || intended_col_num < num_columns-2) {
-                    moveToRow(sanchk.value, sanchk.row_num - 1 > 0? sanchk.row_num - 1 : num_rows - sanchk.row_num - 1);
+                if (sanchk.right.down.value > target_char || intended_col_num < num_columns-2) {
+                    moveToRow(sanchk.right.value, sanchk.right.up.row_num);
                     moveToColumn(expat_value, num_columns - 1);
                     moveToRow(expat_value, characterNodeHashMap.get(expat_value).down.row_num);
                     moveToColumn(target_char, intended_col_num);
-                } else return false;
+                }
+                else{
+                    // FAIL-STATE: Procedure
+                    if (printAll)
+                        System.out.println("Entering Last-Chance mode...:\n" + this);
+                    for (int i=0; i < num_columns/2; i++) {
+                        /* Repeat this sequence until we're back where we started or until the board is solved:
+                        *  Moves: Last Row : Left
+                        *         Last Col : Up
+                        *         Last Row : Left
+                        *         Last Col : Down
+                        * */
+                        moveToRow(sanchk.right.value, sanchk.right.up.row_num);
+                        moveToColumn(sanchk.down.value, sanchk.down.left.col_num);
+                        moveToRow(sanchk.right.value, sanchk.right.down.row_num);
+                        moveToColumn(sanchk.down.value, sanchk.down.left.col_num);
+                        if (sanchk.down.value == target_char && sanchk.down.right.value == solvedBoard[intended_row_num][intended_col_num + 1]) {
+                            if (printAll)
+                                System.out.println("Found a Solution!\n");
+                            return true;
+                        }
+                    }
+                    if (printAll)
+                        System.out.println("Couldn't Find Solution...\n");
+                    if (debug)
+                        interactiveMode(this);
+                    return false;
+                }
             }
         }
         return true;
@@ -354,7 +411,6 @@ public class Loopover {
         //generates random board & it's solution with dimensions between 2x2 & 9x9
         return generateBoards(0, 0);
     }
-
     public static char [][][] generateBoards(int num_rows, int num_columns){
         Random random = new Random();
         if (num_rows < min_num_rows || num_rows > max_num_rows)
@@ -363,7 +419,8 @@ public class Loopover {
             num_columns = random.nextInt(max_num_columns - min_num_columns + 1) + min_num_columns;
 
         char [][][] scrambled_solved_pair_array = new char[2][num_rows][num_columns];
-        char [] sorted_choices = new char[num_rows*num_columns];
+        Character[] custom_sorted_chars = new Character[num_rows * num_columns];
+
         String upper, lower, nums, symbols;
         StringBuilder choices = new StringBuilder(num_rows*num_columns);
         upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -392,34 +449,39 @@ public class Loopover {
                 choices.append(symbols, 0, remaining);
             else choices.append(symbols);
         }
-        choices.getChars(0,choices.length(),sorted_choices,0);
-        Arrays.sort(sorted_choices);
+
+        String str_choices = choices.toString();
+        for (int i=0; i < choices.length(); i++)
+            custom_sorted_chars[i] = str_choices.charAt(i);
+        Arrays.sort(custom_sorted_chars, Node.ValueComparator);
 
         int idx, counter=0;
         while (choices.length()>0){
             idx = random.nextInt(choices.length());
             scrambled_solved_pair_array[0][counter/num_columns][counter%num_columns] = choices.charAt(idx);
-            scrambled_solved_pair_array[1][counter/num_columns][counter%num_columns] = sorted_choices[counter++];
+            scrambled_solved_pair_array[1][counter/num_columns][counter%num_columns] = custom_sorted_chars[counter++];
             choices.deleteCharAt(idx);
         }
         return scrambled_solved_pair_array;
     }
     public static List<String> solve(char[][] mixedUpBoard, char[][] solvedBoard) {
-        boolean printAll = false;
+        return solve(mixedUpBoard, solvedBoard, false);
+    }
+    public static List<String> solve(char[][] mixedUpBoard, char[][] solvedBoard, boolean debug) {
         int num_rows = solvedBoard.length, num_columns = solvedBoard[0].length;
-        Loopover puzzle = new Loopover(mixedUpBoard, printAll);
-        if (printAll)
+        Loopover puzzle = new Loopover(mixedUpBoard, debug);
+        if (debug)
             System.out.println("puzzle BEFORE solve: \n" + puzzle);
         for (int intended_row_num=0; intended_row_num < num_rows; intended_row_num++) {
             for (int intended_col_num=0; intended_col_num < num_columns; intended_col_num++) {
-                if (!puzzle.solvePosition(intended_row_num, intended_col_num, solvedBoard)) {
-                    if (printAll)
+                if (!puzzle.solvePosition(intended_row_num, intended_col_num, solvedBoard, debug)) {
+                    if (debug)
                         System.out.println("puzzle AT time of SURRENDER: \n" + puzzle);
                     return null;
                 }
             }
         }
-        if (printAll)
+        if (debug)
             System.out.println("puzzle AFTER solve: \n" + puzzle);
         return puzzle.movelist;
     }
@@ -435,31 +497,31 @@ public class Loopover {
             if (printAll)
                 System.out.println("Movelist was null. ");
             return solvedBoard==null;
-        }else if (solvedBoard==null) {
+        }
+        else if (solvedBoard==null) {
             if (printAll)
                 System.out.println("No Solved board was provided.\n");
             return false;
         }
-        int num_rows = solvedBoard.length, num_columns=solvedBoard[0].length;
         Node handle = puzzle.characterNodeHashMap.get(mixedUpBoard[0][0]);
-        int rc;
+        int rc_num;
         for (String move : movelist) {
-            rc = Integer.parseInt(move, 1, 2, 10);
+            rc_num = Integer.parseInt(move, 1, 2, 10);
             if (move.charAt(0) == 'L' || move.charAt(0) == 'R')
-                while (handle.row_num != rc)
-                    handle = handle.row_num < rc ? handle.down : handle.up;
+                while (handle.row_num != rc_num)
+                    handle = handle.row_num < rc_num ? handle.down : handle.up;
             else
-                while (handle.col_num != rc)
-                    handle = handle.col_num < rc ? handle.right : handle.left;
+                while (handle.col_num != rc_num)
+                    handle = handle.col_num < rc_num ? handle.right : handle.left;
 
             if (move.charAt(0) == 'L')
-                puzzle.moveToColumn(handle.value, handle.col_num > 0 ? handle.col_num-1 : num_columns-handle.col_num-1);
+                puzzle.moveToColumn(handle.value, handle.left.col_num);
             else if (move.charAt(0) == 'U')
-                puzzle.moveToRow(handle.value, handle.row_num > 0 ? handle.row_num-1 : num_rows-handle.row_num-1);
+                puzzle.moveToRow(handle.value, handle.up.row_num);
             else if (move.charAt(0) == 'R')
-                puzzle.moveToColumn(handle.value, (handle.col_num+1)%num_columns);
+                puzzle.moveToColumn(handle.value, handle.right.col_num);
             else if (move.charAt(0) == 'D')
-                puzzle.moveToRow(handle.value, (handle.row_num+1)%num_rows);
+                puzzle.moveToRow(handle.value, handle.down.row_num);
         }
         for (Node node: puzzle.characterNodeHashMap.values())
             if (node.value != solvedBoard[node.row_num][node.col_num])
@@ -493,6 +555,56 @@ public class Loopover {
         Duration diff = Duration.between(start, end);
         System.out.println("Evaluated "+ n +" puzzles in: " + (diff.getSeconds() + diff.getNano()/1000000000.f) + "s.\n");
         System.out.printf("Avg time per puzzle: %1.9fs\n", diff.dividedBy(n).getSeconds() + diff.dividedBy(n).getNano()/1000000000.f);
+    }
+
+    public static void interactiveMode(Loopover l){
+        System.out.println("\nEntering Interactive mode...\n");
+        Scanner sc = new Scanner(System.in);
+        String instruction;
+        Pattern d = sc.delimiter();
+        Stream<String> s;
+        int id;
+        System.out.println(l);
+        Node curr = l.characterNodeHashMap.values().iterator().next();
+        while (sc.hasNext()) {
+
+            try {
+                sc.useDelimiter("");
+                instruction = sc.next("[a-zA-Z]");
+                id = sc.nextInt();
+                //sc.skip(d);
+                System.out.println("instruction = " + instruction + id);
+
+            } catch (Exception e){
+                sc.next();
+                continue;
+            }
+
+            if (id >= l.num_rows && id >= l.num_columns)
+                continue;
+
+            if (instruction.equalsIgnoreCase("L") && id < l.num_rows) {
+                for (;curr.row_num != id;curr=curr.down);
+                l.moveToColumn(curr.value, curr.left.col_num); //l.rotateRow(id,-1);
+            }
+            else if (instruction.equalsIgnoreCase("R") && id < l.num_rows) {
+                for (;curr.row_num != id;curr=curr.down);
+                l.moveToColumn(curr.value, curr.right.col_num); //l.rotateRow(id,-1);
+            }
+            else if (instruction.equalsIgnoreCase("U") && id < l.num_columns) {
+                for (;curr.col_num != id;curr=curr.right);
+                l.moveToRow(curr.value, curr.up.row_num); //l.rotateRow(id,-1);
+            }
+            else if (instruction.equalsIgnoreCase("D") && id < l.num_columns) {
+                for (;curr.col_num != id;curr=curr.right);
+                l.moveToRow(curr.value, curr.down.row_num); //l.rotateRow(id,-1);
+            }
+            else
+                System.out.println("out of range...");
+            System.out.println(l);
+            System.out.println();
+        }
+        System.out.println("\nLeaving Interactive mode...\n");
     }
     
     public static void main(String [] Args){
@@ -599,18 +711,9 @@ public class Loopover {
                 "ABCDEFGH".toCharArray(),
                 "IJKLMNOP".toCharArray()
         };
-*/
 
-        /* FIXME:
-        *    Currently producing:
-        *       RBCDEA
-        *       GHIJKL
-        *       MNOPQX
-        *       STUVW3
-        *       YZ012F
-        *       456789
-        *   Instead of solvedboard8
-        * */
+
+        // FIXED: SOLVED
         char [][] problem_board8 = {
                 "WCMDJ0".toCharArray(),
                 "ORFBA1".toCharArray(),
@@ -627,13 +730,58 @@ public class Loopover {
                 "YZ0123".toCharArray(),
                 "456789".toCharArray()
         };
+*/
+        /* FIXME:
+        *    Currently producing:
+        *       ABCDEFGH^
+        *       JKLMNOPQR
+        *       STUVWXYZ0
+        *       123456789
+        *       abcdefghi
+        *       jklmnopqr
+        *       stuvwxyz)
+        *       *+,-./:;<
+         *       _=>?@[\]I
+        *   Instead of solvedboard9
+        ** * * * * */
 
-        char [][] mixedupboard, solvedboard;
-        mixedupboard = problem_board8;
-        solvedboard = solvedboard8;
-        List<String> sln = solve(mixedupboard, solvedboard);
-        if (verifySolution(sln, mixedupboard, solvedboard, true))
-            System.out.println("Solution verified correct!\n");
-        else System.out.println("ERR: Board is not in solved state!\n");
+        char [][] problem_board9 = {
+                "vWcEg^TPj".toCharArray(),
+                "5\\_p1I@[r".toCharArray(),
+                "Rf>6;tCA8".toCharArray(),
+                "=nK0zMwU<".toCharArray(),
+                "7B3s,Y*l9".toCharArray(),
+                "yNLoq-+J]".toCharArray(),
+                "hV/F:?XSu".toCharArray(),
+                "x)2QbHimG".toCharArray(),
+                "O.4DZdake".toCharArray()
+        };
+
+        char [][] solvedboard9 = {
+                "ABCDEFGHI".toCharArray(),
+                "JKLMNOPQR".toCharArray(),
+                "STUVWXYZ0".toCharArray(),
+                "123456789".toCharArray(),
+                "abcdefghi".toCharArray(),
+                "jklmnopqr".toCharArray(),
+                "stuvwxyz)".toCharArray(),
+                "*+,-./:;<".toCharArray(),
+                "=>?@[\\]^_".toCharArray()
+        };
+
+
+        if (true){
+            regressionTest();
+        }
+        else {
+            char[][] mixedupboard, solvedboard;
+            mixedupboard = problem_board9;
+            solvedboard = solvedboard9;
+
+            List<String> sln = solve(mixedupboard, solvedboard, true);
+            if (verifySolution(sln, mixedupboard, solvedboard, true))
+                System.out.println("Solution verified correct!\n");
+            else System.out.println("ERR: Board is not in solved state!\n");
+        }
     }
 }
